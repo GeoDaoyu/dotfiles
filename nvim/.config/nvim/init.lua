ee2b6bc0840eb06e3983903d81e510ed1c6f6567
zhanggy <zhanggy@geoscene.cn>
2024-07-30 18:49:00 +0800

    fix: options.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.cmd("set nobackup")
-vim.cmd("set noswapfile")
-vim.cmd("set noundofile")
-vim.cmd("set nowritebackup")
