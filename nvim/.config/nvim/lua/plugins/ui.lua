0eeace20995c7f35d0fe10abc0ca38b70e5459ad
zhanggy <zhanggy@geoscene.cn>
2024-08-20 09:58:33 +0800

    fix: neo-tree filter hidden

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -19,6 +19,18 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       sources = { "filesystem" },
+      filesystem = {
+        filtered_items = {
+          visible = false, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = true, -- only works on Windows for hidden files/directories
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            ".DS_Store",
+            ".git",
+          },
+        },
+      },
     },
   },
 }
