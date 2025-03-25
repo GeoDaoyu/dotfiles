6c6394ed5fcb0842962a9fbfb28f34116e9072c5
zhanggy <zhanggy@geoscene.cn>
2025-03-25 10:04:40 +0800

    feat: use my snippets
    copy from 'friendly-snippets'

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "rafamadriz/friendly-snippets",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+    end,
+  },
+  {
     "blink.cmp",
     opts = {
       keymap = {
@@ -11,6 +17,26 @@ return {
       completion = {
         documentation = {
           auto_show = false,
+        },
+      },
+      sources = {
+        providers = {
+          lsp = {
+            max_items = 3,
+            score_offset = 100,
+          },
+          snippets = {
+            max_items = 5,
+            score_offset = 90,
+          },
+          buffer = {
+            max_items = 3,
+            score_offset = 80,
+          },
+          path = {
+            max_items = 3,
+            score_offset = 70,
+          },
         },
       },
     },
